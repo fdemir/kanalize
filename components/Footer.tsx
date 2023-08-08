c156916a97f61b4332d4a21c5c842fe8045d72fd
fdemir <frndemir@gmail.com>
2023-08-08 03:38:13 +0300

    feat: update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,25 @@
 export default function Footer() {
   return (
-    <div className="py-4">
-      <span>
-        Powered by OpenAI, Langchain and Fresh
-      </span>
+    <div className="py-4 flex justify-between items-center w-full">
+      <div>
+        <span>
+          Powered by OpenAI, Langchain and Fresh
+        </span>
+        <br />
+        <span className="text-gray-600">
+          Check out the{" "}
+          <a className="text-underline text-green-600">Dokusearch</a>{" "}
+          project to talk with documents.
+        </span>
+      </div>
+      <div className="flex gap-2">
+        <a href="https://github.com/fdemir/kanalize">
+          <img src="/github.svg" className="w-8 inline-block" />
+        </a>
+        <a href="https://twitter.com/furkaneulogy" target="_blank">
+          <img src="/twitter.svg" className="w-8 inline-block" />
+        </a>
+      </div>
     </div>
   );
 }
--- routes/index.tsx
+++ routes/index.tsx
@@ -16,7 +16,7 @@ export default function Home() {
           <Header />
           <Generator />
         </div>
-        <div className="container gap-4 px-0 md:px-20">
+        <div className="container mx-auto py-4 gap-4 px-0 md:px-20 ">
           <Footer />
         </div>
       </main>
