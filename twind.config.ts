2000ba8abecf2824fa39e490d5d1ad7f1defb100
fdemir <frndemir@gmail.com>
2023-08-12 05:50:22 +0300

    style: change container padding

--- twind.config.ts
+++ twind.config.ts
@@ -2,5 +2,15 @@ import { Options } from "$fresh/plugins/twind.ts";
 
 export default {
   selfURL: import.meta.url,
-  theme: {},
+  theme: {
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "14rem",
+      },
+    },
+  },
 } as Options;
