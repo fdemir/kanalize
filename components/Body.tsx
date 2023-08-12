3017a16b2ded24a43bb51e295bb449eb563a60e4
fdemir <frndemir@gmail.com>
2023-08-12 05:51:03 +0300

    style: apply container new spacings

--- components/Body.tsx
+++ components/Body.tsx
@@ -2,18 +2,20 @@ import Generator from "../islands/Generator.tsx";
 import Header from "../components/Header.tsx";
 import Footer from "../components/Footer.tsx";
 import Examples from "../islands/Examples.tsx";
+import HowItWorks from "./HowItWorks.tsx";
 
 export default function Body(props: {
   slug?: string;
 }) {
   return (
     <main className="min-h-screen bg-gray-200 w-full md:px-0 px-8">
-      <div className="container mx-auto py-4 gap-4 px-0 md:px-20 min-h-screen">
+      <div className="container mx-auto py-4 gap-4 min-h-screen flex flex-col items-center">
         <Header />
         <Generator defaultSkill={props.slug} />
       </div>
 
-      <div className="container mx-auto py-4 gap-4 px-0 md:px-20 ">
+      <div className="container mx-auto py-4 gap-4 px-0  ">
+        <HowItWorks />
         <Examples />
         <Footer />
       </div>
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col items-center">
       <h1 className="text-3xl font-bold mb-3">Kanalize</h1>
       <h2 className="text-gray-600 xl:w-8/12 w-full text-xl">
         Kanalize is a simple tool to generate open-source project ideas for
