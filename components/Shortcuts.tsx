22e2dfcff2cc018dd89528a2773c881461d626c9
fdemir <frndemir@gmail.com>
2023-08-20 23:21:48 +0300

    feat: use kbd instead of span

--- components/Shortcuts.tsx
+++ components/Shortcuts.tsx
@@ -2,21 +2,21 @@ export default function Shortcuts() {
   return (
     <div className="gap-1 flex text-opacity-50 w-full items-center justify-center">
       <div className="bg-white bg-opacity-5 rounded-md p-2 gap-1  inline-flex text-sm">
-        <span className="w-5 h-5 flex items-center justify-center bg-white bg-opacity-10 rounded-md">
+        <kbd className="w-5 h-5 flex items-center justify-center bg-white bg-opacity-10 rounded-md">
           I
-        </span>
+        </kbd>
         <span>Info</span>
       </div>
       <div className="bg-white bg-opacity-5 rounded-md p-2 gap-1  inline-flex text-sm">
-        <span className="w-5 h-5 flex items-center justify-center bg-white bg-opacity-10 rounded-md">
+        <kbd className="w-5 h-5 flex items-center justify-center bg-white bg-opacity-10 rounded-md">
           ←
-        </span>
+        </kbd>
         <span>Dislike</span>
       </div>
       <div className="bg-white bg-opacity-5 rounded-md p-2 gap-1  inline-flex text-sm">
-        <span className="w-5 h-5 flex items-center justify-center bg-white bg-opacity-10 rounded-md">
+        <kbd className="w-5 h-5 flex items-center justify-center bg-white bg-opacity-10 rounded-md">
           →
-        </span>
+        </kbd>
         <span>Like</span>
       </div>
     </div>
